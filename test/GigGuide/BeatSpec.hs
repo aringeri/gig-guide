390a076655e1abc6aa9e225eea99523039cdda04
aringeri <ac.ringeri@gmail.com>
2025-04-21 16:26:51 +1000

    Start test for parsing event overviews

--- test/GigGuide/BeatSpec.hs
+++ test/GigGuide/BeatSpec.hs
@@ -2,6 +2,7 @@
 module GigGuide.BeatSpec where
 
 import Data.Time (fromGregorian)
+import Data.Either()
 import GigGuide.Scraper.BeatScraper
 import Control.Monad.Logger
 import Control.Monad.Identity
@@ -9,6 +10,8 @@ import GigGuide.Scraper.Beat.Internal(parseDay, EventOverview(..))
 import Text.HTML.Scalpel
 import Test.Hspec
 import qualified Data.Text.Lazy as L
+import GigGuide.Types.EventOverview (EventCategory(..))
+import GigGuide.Types (mkPriceRange)
 
 type EventOverviewScrapeResult = Maybe [Either EventCreationError EventOverview]
 
@@ -26,7 +29,7 @@ spec = do
   before (L.pack <$> readFile "data/responses/simple-event-overview.html") $ do
     describe "event overview scraper" $ do
       it "should parse event overviews" $ \f -> do
-        parseEventOverviews f `shouldBe` (Nothing, [])
+        parseEventOverviews f `shouldBe` (expectedEventOverviews, [])
         where 
           parseEventOverviews :: L.Text -> (EventOverviewScrapeResult, [LogLine])
           parseEventOverviews str = 
@@ -34,4 +37,32 @@ spec = do
 
           scrapeEventOverviews :: MonadLogger m => L.Text ->  m EventOverviewScrapeResult
           scrapeEventOverviews str = scrapeStringLikeT str eventOverviews 
+
+          expectedEventOverviews :: Maybe [Either EventCreationError EventOverview]
+          expectedEventOverviews = pure [
+              pure (EventOverview
+                  "Dangerous Goods &#038; Aura present Astrix &#038; Victor Ruiz"
+                  "https://beat.com.au/dangerous-goods-aura-present-astrix-victor-ruiz/"
+                  (fromGregorian 2025 04 18)
+                  [Music]
+                  ""
+                  (Left 149.05)
+                )
+              , pure (EventOverview
+                  "Main Stage Festival Showcase"
+                  "https://beat.com.au/main-stage-festival-showcase-2/"
+                  (fromGregorian 2025 04 18)
+                  [Comedy]
+                  "https://beat.com.au/directory/comedy-republic/"
+                  (Right $ mkPriceRange 23.50 25.50)
+                )
+              , pure (EventOverview
+                  "Two Hearts: Heartcore Karaoke"
+                  "https://beat.com.au/two-hearts-heartcore-karaoke-2/"
+                  (fromGregorian 2025 04 18)
+                  [Music, Comedy]
+                  "https://beat.com.au/directory/comedy-republic/"
+                  (Right $ mkPriceRange 28.50 31.50)
+                )
+            ]
         
